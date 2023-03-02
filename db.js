88915702503b9be09b68b1282b2f5ed2310fffa3
Yurderie <emreyurderi@gmail.com>
2023-03-01 23:34:18 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 module.exports =
  {
  //"URI":"mongodb://127.0.0.1:27017/book_store"
- "URI":"mongodb://localhost/emreyurderiassignment"
+ //"URI":"mongodb://localhost/emreyurderiassignment"
+ "URI":"mongodb+srv://emre:emre12345@cluster0.tgyy6wm.mongodb.net/?retryWrites=true&w=majority"
 }
